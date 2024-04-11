a2e5c6b03c6dec817f0785e0b3d07d25026d39aa
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2024-04-11 20:36:03 +0200

    Vytvoření obecné obsluhy dialogů

--- html_sablony/script.js
+++ html_sablony/script.js
@@ -13,3 +13,57 @@ zavritMobilniNavigaci.addEventListener('click', () => {
     otevritMobilniNavigaci.classList.remove('otevreno');
     zavritMobilniNavigaci.classList.remove('otevreno');
 });
+
+
+/*
+Zobrazit dialogové okno:
+- po kliknutí na nejaký element se dialog otevře
+- po kliknutí na nějaký element se dialog zavře (tlačítko, ikona - křížek, apod.)
+- po kliknutí na backdrop se dialog zavře
+
+- identifikace elemnetů pro otevření/zavření bude realizována pomocí atributového (CSS) selektoru
+- dialog indentifikujeme pomocí ID atributu
+- element, který bude otevírat/zavírat dialog bude uloženo ID dialogu v data atributu
+
+<button data-otevri-dialog="info">Otevři dialog</button>
+
+<dialog id="info">
+    <!-- div je potřeba pro správnou funkcionalitu zavření při kliknutí do backdropu -->
+    <div class="obsah-dialogu">
+        text zprávy
+        <button data-zavri-dialog="info">Zavri dialog</button>
+    </div>        
+</dialog>
+*/
+
+const otevriDialog = document.querySelectorAll("[data-otevri-dialog]");
+const zavriDialog = document.querySelectorAll("[data-zavri-dialog]");
+const dialogy = document.getElementsByTagName('dialog');
+
+console.log(otevriDialog, zavriDialog, dialogy);
+
+/* vytvoření posluchačů událostí pro otevření - libovolný počet */
+for (let i = 0; i < otevriDialog.length; i++) {
+    otevriDialog[i].addEventListener('click', (event) => {
+        const dialogId = event.target.dataset.otevriDialog;
+        document.getElementById(dialogId).showModal();
+    });
+}
+
+/* vytvoření posluchačů událostí pro zavření - libovolný počet */
+for (let i = 0; i < zavriDialog.length; i++) {
+    zavriDialog[i].addEventListener('click', (event) => {
+        const dialogId = event.target.dataset.zavriDialog;
+        document.getElementById(dialogId).close();
+    });
+}
+
+/* vytvoření posluchačů událostí pro zavření při kliknutí do backdropu - nutné i nastavení v CSS */
+for (let i = 0; i < dialogy.length; i++) {
+    dialogy[i].addEventListener('click', (event) => {
+        if (event.target === event.currentTarget) {
+            event.target.close();
+        }
+    });
+}
+
