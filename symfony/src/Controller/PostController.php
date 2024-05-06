2be2ac6315d6829a70325bb1c1a3b98c7ecff95e
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2024-05-06 19:52:35 +0200

    Vytvoření stránkovaného výpisu článků

--- symfony/src/Controller/PostController.php
+++ symfony/src/Controller/PostController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use App\Repository\PostRepository;
+use App\Utils\PaginationData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,19 +17,21 @@ class PostController extends AbstractController
     }
 
     #[Route('/prispevky/{slug}', name: 'app_posts')]
-    public function index(CategoryRepository $categoryRepository, string $slug = ''): Response
+    public function index(Request $request, CategoryRepository $categoryRepository, string $slug = ''): Response
     {
         if ($slug) {
             $category = $categoryRepository->findOneBy(['slug' => $slug]);
-            $posts = $this->postRepository->findBy(['category' => $category], ['id' => 'DESC']);
-        } else {
-            $posts = $this->postRepository->findBy([], ['id' => 'DESC']);
         }
 
-        dump($posts);
+        $page = max(1, $request->query->getInt('page', 1));
+        $offset = ($page - 1) * PostRepository::POSTS_PER_PAGE;
+        $paginator = $this->postRepository->getPaginatior($category ?? null, $offset);
+        $paginationData = new PaginationData(PostRepository::POSTS_PER_PAGE, count($paginator), $page);
 
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'category' => $category ?? null,
+            'posts' => $paginator,
+            'pagination_data' => $paginationData,
         ]);
     }
 }
